607e7ee84ac188420e5b14a0735e83bf72a9f65e
Bionca Bond <88898665+bioncabond@users.noreply.github.com>
2024-11-05 15:14:22 -0800

    Update rotateArray.js
    Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.

--- 01_arrays/rotateArray.js
+++ 01_arrays/rotateArray.js
@@ -1,4 +1,6 @@
 /**
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
  * @param {number[]} nums
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
